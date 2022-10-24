a9fb1b766bb6d7c5f7374cb28b9d38d1f3bd538c
Vladimir <falinvlad93@rambler.ru>
2022-10-24 14:02:39 +0300

    fix filter tags, fix server

--- controllers/PostControllers.js
+++ controllers/PostControllers.js
@@ -106,7 +106,7 @@ export const create = async (req, res) => {
             title: req.body.title,
             text: req.body.text,
             imageUrl: req.body.imageUrl,
-            tags: req.body.tags.split(','),
+            tags: req.body.tags.split(',').map(item => item.trim()),
             user: req.userId,
             imageAvatarUrl: imageAvatar
         });
@@ -165,10 +165,76 @@ export const getLastTags = async (req, res) => {
 
 export const getTagsFilter = async (req, res) => {
   try{
-    const tags = req.params
-    const posts = await PostModel.find().exec();
+    // const page = parseInt(req.query.page) - 1 || 0;
+		// const limit = parseInt(req.query.limit) || 5;
+		// const search = req.query.search || "";
+		// let sort = req.query.sort || "rating";
+		// let genre = req.query.genre || "All";
 
-    res.json(posts);
+		// const genreOptions = [
+		// 	"Action",
+		// 	"Romance",
+		// 	"Fantasy",
+		// 	"Drama",
+		// 	"Crime",
+		// 	"Adventure",
+		// 	"Thriller",
+		// 	"Sci-fi",
+		// 	"Music",
+		// 	"Family",
+		// ];
+
+		// genre === "All"
+		// 	? (genre = [...genreOptions])
+		// 	: (genre = req.query.genre.split(","));
+		// req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
+
+		// let sortBy = {};
+		// if (sort[1]) {
+		// 	sortBy[sort[0]] = sort[1];
+		// } else {
+		// 	sortBy[sort[0]] = "asc";
+		// }
+
+		// const movies = await Movie.find({ name: { $regex: search, $options: "i" } })
+		// 	.where("genre")
+		// 	.in([...genre])
+		// 	.sort(sortBy)
+		// 	.skip(page * limit)
+		// 	.limit(limit);
+
+		// const total = await Movie.countDocuments({
+		// 	genre: { $in: [...genre] },
+		// 	name: { $regex: search, $options: "i" },
+		// });
+
+		// const response = {
+		// 	error: false,
+		// 	total,
+		// 	page: page + 1,
+		// 	limit,
+		// 	genres: genreOptions,
+		// 	movies,
+		// };
+
+		// res.status(200).json(response);
+
+    const tagsUrl = req.params.id
+    // const tags = req.body.tags
+
+    const tags = await PostModel.find({ tags: tagsUrl });
+  
+
+  //   const total = await PostModel.countDocuments({
+  //     tags: { $in: [...tags] },
+  //   });
+
+  // const response = {
+  //   total,
+  //   tags: tagsUrl,
+  //   posts
+  // };
+    res.json(tags);
   } catch(err) {
       console.log(err);
       res.status(404).json({
